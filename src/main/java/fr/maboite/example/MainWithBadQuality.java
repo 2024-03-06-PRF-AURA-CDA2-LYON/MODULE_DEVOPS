63a63c56c210f6c3587fe39400296315250d079e
François Caremoli <francois.caremoli@wanadoo.fr>
2024-09-29 22:26:36 +0200

    Premier commit

---
+++ src/main/java/fr/maboite/example/MainWithBadQuality.java
@@ -0,0 +1,47 @@
+package fr.maboite.example;
+
+public class MainWithBadQuality {
+
+	private int startValue;
+
+	private void setStartValue(int startValue) {
+		this.startValue = startValue;
+	}
+
+	public static void main(String[] args) {
+		MainWithBadQuality badQuality = new MainWithBadQuality();
+		badQuality.setStartValue(12);
+		badQuality.compute();
+
+	}
+
+	public int compute() {
+
+		for (int i = 0; i < 3; i++) {
+			int k = 3;
+			k *= i;
+			System.out.println(k);
+		}
+
+		System.out.println(2 * startValue);
+		for (int i = 0; i < 3; i++) {
+			int k = 3;
+			k *= i;
+			System.out.println(k);
+		}
+		int j = 12;
+		String superString = "sds";
+		boolean maybe = true;
+		if (maybe) {
+			superString = null;
+		}
+		System.out.println(superString.toCharArray());
+
+		if (this.startValue <= 0) {
+			return 0;
+		} else {
+			return this.startValue * 2;
+		}
+	}
+
+}
